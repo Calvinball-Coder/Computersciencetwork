6bdadc650c970d593429c653869df7fcd5fd91ca
LAB <LAB>
2017-08-24 11:47:55 -0500

    I wrote this

---
+++ SumNumCheck.java
@@ -0,0 +1,11 @@
+public class SumNumCheck
+{
+	public static void main(String[] args) {
+	System.out.println("Does This work?");
+        int count = 1;
+        while (count < 11) {
+            System.out.println("Count is: " + count);
+            count++;
+        }
+	}
+}
